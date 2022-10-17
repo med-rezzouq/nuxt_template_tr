66b4e77273ef3045aa721698a5084484489fed8b
rezzouq <mohammed.rezzouq@siway.ma>
2022-10-17 22:00:53 +0100

    build(first commit):

---
+++ components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <img class="NuxtLogo" width="245" height="180" src="images/logo.png" alt="" />
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
